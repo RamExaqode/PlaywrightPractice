15768d420fe4392459d858433649eb438531ffd1
Ram QA <ram.marshivane@exaqode.com>
2024-06-02 16:28:00 +0530

    Added Allure Reports along with screenshot and video on every step

---
+++ tests/HomepageTest.spec.js
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test('Home Page',async ({page})=>
+    //execution will not go stpe by step in asynchroniously
+{
+    await page.goto('https://demoblaze.com/');
+ //await - wait for the function till the page is loaded
+ 
+ const pageTitle=await page.title();
+ console.log("Title of the page is: ", pageTitle);
+ await expect(page).toHaveTitle('STORE');
+
+ const pageURl=page.url();
+ console.log("Page URL is:", pageURl);
+ await expect(page).toHaveURL("https://demoblaze.com/");
+
+//await page.close();
+
+
+})

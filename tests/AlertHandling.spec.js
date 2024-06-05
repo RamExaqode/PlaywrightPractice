6a580896aeea5978c048dc8398c431783dabb56e
Ram QA <ram.marshivane@exaqode.com>
2024-06-06 02:32:22 +0530

    Alert handling

---
+++ tests/AlertHandling.spec.js
@@ -0,0 +1,74 @@
+import { chromium, test, expect } from '@playwright/test';
+import exp from 'constants';
+import { skip } from 'node:test';
+
+test('Alert Handling', async ({page}) => {
+   
+    await page.goto('https://testautomationpractice.blogspot.com/');
+
+    //Enabling alert handling
+    page.on('dialog', async dialog =>{
+        expect(dialog.type()).toContain('alert')
+        expect(dialog.message()).toContain('I am an alert box');
+        await page.waitForTimeout(3000);
+        await dialog.accept();
+
+    })
+    await page.click("//button[@onclick='myFunctionAlert()']");
+
+await page.waitForTimeout(5000);
+
+});
+
+test('Confirmation Dialogbox Ok or Cancel Handling', async ({page}) => {
+   
+    //await page.goto('https://testautomationpractice.blogspot.com/');
+
+    // Refresh the page
+    await page.reload();
+
+    // Enabling alert handling
+    page.on('dialog', async dialog => {
+        expect(dialog.type()).toBe('confirm');
+        expect(dialog.message()).toContain('Press a button!');
+        await page.waitForTimeout(3000);
+        await dialog.accept(); // Accept the confirmation dialog
+        // If you want to test the cancel functionality, uncomment the following line and comment the above line
+        // await dialog.dismiss(); 
+    });
+
+    // Triggering the confirmation dialog
+    await page.click("//button[@onclick='myFunctionConfirm()']");
+
+    // Verify the text after accepting the dialog
+    await expect(page.locator('#demo')).toHaveText('You pressed OK!');
+    
+    await page.waitForTimeout(5000);
+});
+
+test('Prompt alert Handling', async ({page}) => {
+   
+    //await page.goto('https://testautomationpractice.blogspot.com/');
+
+    // Refresh the page
+    await page.reload();
+    
+    // Enabling alert handling
+    page.on('dialog', async dialog => {
+        expect(dialog.type()).toBe('prompt');
+        expect(dialog.message()).toContain('Please enter your name:');
+        expect(dialog.defaultValue('Harry Potter'))
+        await page.waitForTimeout(3000);
+        await dialog.accept('Ram Marshivane'); // Accept the confirmation dialog
+        // If you want to test the cancel functionality, uncomment the following line and comment the above line
+        // await dialog.dismiss(); 
+    });
+
+    // Triggering the confirmation dialog
+    await page.click("//button[@onclick='myFunctionPrompt()']");
+
+    // Verify the text after accepting the dialog
+    await expect(page.locator('#demo')).toHaveText('Hello Ram Marshivane! How are you today?');
+    
+    await page.waitForTimeout(5000);
+});
---
+++ tests/AutoSuggestDropdown.spec.js
@@ -0,0 +1,54 @@
+import { chromium, test, expect } from '@playwright/test';
+
+test('Dropdown Handling', async () => {
+    const browser = await chromium.launch({
+        args: ['--disable-http2']
+    });
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    await page.goto('https://www.redbus.in/');
+
+    //From location
+    await page.locator('#src').fill('Pune');
+
+    // Wait for the options to appear
+    await page.waitForSelector("//li[@class='sc-iwsKbI jTMXri']/div");
+
+    // Select all options in the dropdown
+    const fromCityOptions = await page.$$("li.sc-iwsKbI.jTMXri div");
+
+    for (let option of fromCityOptions) {
+        const value = await option.textContent();
+        console.log(value);
+
+        if (value.includes('Swargate Pune')) { 
+            await option.click(); // Click the element, not the value
+            break; // Exit loop after finding the correct option
+        }
+    }
+
+    //Destination Location
+
+    await page.locator('#dest').fill('Nanded');
+
+    // Wait for the options to appear
+    await page.waitForSelector("//li[@class='sc-iwsKbI jTMXri']/div");
+
+    // Select all options in the dropdown
+    const destityOptions = await page.$$("li.sc-iwsKbI.jTMXri div");
+
+    for (let option1 of destityOptions) {
+        const value2 = await option1.textContent();
+        console.log(value2);
+
+        if (value2.includes('Old Mondha')) { 
+            await option1.click(); // Click the element, not the value
+            break; // Exit loop after finding the correct option
+        }
+    }
+
+    
+
+    await page.waitForTimeout(5000);
+});
